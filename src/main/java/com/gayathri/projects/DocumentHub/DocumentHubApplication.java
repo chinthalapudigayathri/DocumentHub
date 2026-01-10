e783833da67ba66dfaf4bec219f2118b23b6fd83
chinthalapudigayathri <gayathri.ch.sde@gmail.com>
2026-01-10 11:27:09 +0530

    Initial commit

---
+++ src/main/java/com/gayathri/projects/DocumentHub/DocumentHubApplication.java
@@ -0,0 +1,13 @@
+package com.gayathri.projects.DocumentHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gayathri/projects/DocumentHub/DocumentHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gayathri.projects.DocumentHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
