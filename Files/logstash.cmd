3717c2a33682ab4baca59adfd001e4a9dd9402fa
chinthalapudigayathri <gayathri.ch.sde@gmail.com>
2026-01-17 19:59:13 +0530

    updating logstash file to log on elastic search and kibana

--- Files/logstash.cmd
+++ Files/logstash.cmd
@@ -1,2 +1,6 @@
 #go to /logstash/bin
-logstash.bat -f C:\Tools\Collab\Logstash\logstash-9.2.3\config
+#logstash.bat -f C:\Tools\Collab\Logstash\logstash-9.2.3\config
+#Run the following command only
+logstash -f config/logstash.conf
+# you can check the log on kibana
+http://localhost:9200/_cat/indices?v
