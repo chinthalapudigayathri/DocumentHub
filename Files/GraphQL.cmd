1858a18905aae2c7186f8685659e153eb90bbd65
chinthalapudigayathri <gayathri.ch.sde@gmail.com>
2026-01-19 20:45:22 +0530

    updating changes for GraphQL

---
+++ Files/GraphQL.cmd
@@ -0,0 +1,7 @@
+#springboot automatically links url to
+http://localhost:8080/graphql/?continue
+
+#To test go to
+http://localhost:8080/graphiql?path=/graphql
+#or
+http://localhost:8080/graphql-ui
